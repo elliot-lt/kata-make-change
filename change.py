b38b81deec8fa5bcd6fc1c0db4df0a03c1c36ad4
Elliot Iddon <elliot@land.tech>
2022-10-06 16:21:42 +0100

    Initial setup

---
+++ change.py
@@ -0,0 +1,6 @@
+from typing import List
+# Representing the number of cents in valid euro coins/notes
+valid_denominations = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000]
+
+def make_change(amount: int) -> List[int]:
+  pass
---
+++ test_change.py
@@ -0,0 +1,4 @@
+import change
+
+def test_make_change():
+  change.make_change(7)
